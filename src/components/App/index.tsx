41949152fa3ea0d2ae552632b304c1d46853d661
UnicornSnafy <ya.shirokov@icloud.com>
2020-06-22 09:00:44 +0500

    UPDATE - Добавил дата-пикер и обновил свитч

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,17 @@
 import React, { useState } from "react";
 import Switch from "../Switch";
+import styled from "styled-components";
+import DataPicker from "../Datapicker";
+
+const Form = styled.div`
+  position: relative;
+  max-width: 900px;
+  padding: 50px;
+  margin: 50px;
+  border-radius: 25px;
+  background-color: #fcfcfc;
+  box-shadow: 0 0 50px rgba(0, 0, 0, 0.15);
+`;
 
 function App() {
   const [state, setState] = useState({
@@ -7,16 +19,23 @@ function App() {
   });
 
   return (
-    <div>
-      <label>
-        <Switch
-          handleChange={(value: boolean) =>
-            setState({ ...state, switch: value })
-          }
-        />
-        <span>{state.switch.toString()}</span>
-      </label>
-    </div>
+    <>
+      <Form>
+        <h1>Delta UI</h1>
+        <div>
+          <Switch
+            title={state.switch.toString()}
+            value={state.switch}
+            handleChange={(value: boolean) =>
+              setState({ ...state, switch: value })
+            }
+          />
+        </div>
+        <div>
+          <DataPicker />
+        </div>
+      </Form>
+    </>
   );
 }
 
--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,22 +1,52 @@
-import React, { useState } from "react";
+import React from "react";
+import { styled } from "../../theme";
+
+const Label = styled.label`
+  display: inline-flex;
+  cursor: pointer;
+`;
+
+const Title = styled.div`
+  margin-left: 5px;
+`;
+
+const ButtonWrapper = styled.div<{ active: boolean }>`
+  display: inline-block;
+  width: 48px;
+  height: 24px;
+  border-radius: 12px;
+  background-color: ${(props) =>
+    props.active ? props.theme.colors.color2 : props.theme.colors.color1};
+  transition-duration: 0.2s;
+`;
+
+const Button = styled.div<{ active: boolean }>`
+  width: 18px;
+  height: 18px;
+  margin: ${(props) => (props.active ? "3px 3px 3px 26px" : "3px")};
+  border-radius: 50%;
+  background-color: #ffffff;
+  transition-duration: 0.2s;
+`;
 
 type Props = {
+  title: string;
   handleChange: (value: boolean) => void;
-  defaultValue?: boolean;
+  value: boolean;
 };
 
-const Switch = ({ handleChange, defaultValue = false }: Props) => {
-  const [value, setValue] = useState(defaultValue);
-
+const Switch = ({ title, handleChange, value }: Props) => {
   return (
-    <div
+    <Label
       onClick={() => {
         handleChange(!value);
-        setValue(!value);
       }}
     >
-      <div></div>
-    </div>
+      <ButtonWrapper active={value}>
+        <Button active={value} />
+      </ButtonWrapper>
+      <Title>{title}</Title>
+    </Label>
   );
 };
 

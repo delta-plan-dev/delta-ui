9b86a2c08c304bfedcd9a3a9d35c6cb8e85f2a26
KawaiiSnafy <ya.shirokov@icloud.com>
2020-12-04 01:31:02 +0500

    UPDATE: глобальный стиль

--- src/global-style.ts
+++ src/global-style.ts
@@ -14,17 +14,33 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+  
   code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
       monospace;
   }
+  
   #root {
+    display: flex;
+    flex-direction: column;
     position: absolute;
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
   }
+  
+  #root main {
+    position: relative;
+    overflow: auto;
+    flex-grow: 1;
+  }
+   
   h1, h2, h3, h4, h5, h6 {
     margin-top: 0;
     margin-bottom: .75rem;
+  }
+  
+  ul {
+    padding: 0;
+    list-style-type: none;
   }
 `;

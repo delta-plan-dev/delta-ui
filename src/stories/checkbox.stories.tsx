22c33d8b4c457f94dba1f5e32974ae2abca47842
KawaiiSnafy <ya.shirokov@icloud.com>
2020-12-25 14:08:36 +0500

    FIX: name of stories

--- src/stories/checkbox.stories.tsx
+++ src/stories/checkbox.stories.tsx
@@ -3,13 +3,13 @@ import { Story } from '@storybook/react';
 import { CheckBox } from '../components/checkbox';
 
 const Template: Story<ComponentProps<typeof CheckBox>> = (args) => (
-    <CheckBox {...args}/>
-)
+  <CheckBox {...args} />
+);
 
 export const Default = Template.bind({});
-Default.args = {}
+Default.args = {};
 
 export default {
-    title: "Checkbox.",
-    component: CheckBox
-}
+  title: 'Checkbox',
+  component: CheckBox,
+};
--- src/stories/radio.stories.tsx
+++ src/stories/radio.stories.tsx
@@ -3,13 +3,13 @@ import { Story } from '@storybook/react';
 import { Radio } from '../components/radio';
 
 const Template: Story<ComponentProps<typeof Radio>> = (args) => (
-    <Radio {...args}/>
-)
+  <Radio {...args} />
+);
 
 export const Default = Template.bind({});
-Default.args = {}
+Default.args = {};
 
 export default {
-    title: "Radio.",
-    component: Radio
-}
+  title: 'Radio',
+  component: Radio,
+};

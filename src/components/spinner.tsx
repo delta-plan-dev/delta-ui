7b916743e83e6c549cf4721b3cd8db26ad91aa87
neoru4 <ibiws0373@yandex.ru>
2021-03-28 22:15:30 +0500

    UPDATE: add spinner, next wave

---
+++ src/components/spinner.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface IProps {
+  size?: string | number;
+  beam?: string | number;
+}
+
+const StyledSpinner = styled('span')<IProps>`
+  @keyframes spinner-border {
+    to {
+      transform: rotate(1turn);
+    }
+  }
+
+  display: inline-block;
+  vertical-align: text-bottom;
+  border: ${(props: IProps) => {
+    const beam = props.beam;
+    const sizeUnit = typeof beam == 'number' ? `${beam}px` : beam;
+    return `${sizeUnit} solid;  border-right: ${sizeUnit} solid transparent;`;
+  }}
+    
+  border-radius: 50%;
+  animation: spinner-border 0.75s linear infinite;
+  ${(props: IProps) => {
+    const size = props.size;
+    const sizeUnit = typeof size == 'number' ? `${size}px` : size;
+    return `width: ${sizeUnit}; height: ${sizeUnit}`;
+  }}
+`;
+
+export const Spinner: React.FC<IProps> = (props) => {
+  const { size = 25, beam = 4 } = props;
+  return <StyledSpinner size={size} beam={beam} {...props} />;
+};
---
+++ src/stories/spinner.stories.tsx
@@ -0,0 +1,17 @@
+import React, { ComponentProps } from 'react';
+import { Story } from '@storybook/react';
+import { Spinner } from '../components/spinner';
+
+const Template: Story<ComponentProps<typeof Spinner>> = (props) => {
+  return (
+    <>
+      <Spinner {...props} />
+      <Spinner />
+    </>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = { size: 30 };
+
+export default { title: 'Spinner', component: Spinner };
